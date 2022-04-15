44207bd96bb8fcddf48efc7e029698a2aa84f6bc
Nathan <nathancai1031@gmail.com>
2022-04-14 23:31:37 -0400

    cleared index page

--- frontend/components/SearchBox.tsx
+++ frontend/components/SearchBox.tsx
@@ -1,17 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import { API_URL } from "../config";
 
 export default function SearchBox() {
-    const [show, setShow] = useState(false)
+    const [show, setShow] = useState(false);
+    const [locations, setLocations] = useState([]);
+
+    // useEffect(() => {
+    //     (async () => {
+    //         const data = await fetch(`${API_URL}/stores/locations`);
+    //         const res = await data.json();
+
+    //         setLocations(res);
+    //     })()
+    // }, [])
 
     return (
         <div>
-            <h3>
+            <h3 onClick={() => setShow(true)}>
                 <FaSearch /> Find a store
             </h3>
-            {show && (<div>
-                
-            </div>)}
+            {show && <div></div>}
         </div>
     );
 }
