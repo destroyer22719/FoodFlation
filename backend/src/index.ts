d5d8fed57d50c57681c089c9bf149651949e4121
Nathan <nathancai1031@gmail.com>
2022-04-15 13:29:17 -0400

    added to backend  morgan and cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import sequelize from "./config/db.js";
+import morgan from "morgan";
+import cors from "cors";
 
 import itemRouter from "./routes/items.js";
 import storeRouter from "./routes/stores.js";
@@ -10,6 +12,10 @@ const port = process.env.PORT || 4000;
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(morgan("tiny"));
+
 
 app.use("/items", itemRouter);
 app.use("/stores", storeRouter);
