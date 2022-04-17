0a1c9a8a4104ed05ad8be8c3af913778f34599bb
Nathan <nathancai1031@gmail.com>
2022-04-16 22:10:17 -0400

    Created basic page for index

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return (
-    <div>
+    return (
+        <div>
+            <h1>FoodFLation</h1>
+            <p>Tracking prices in different stores around Canada</p>
+        </div>
+    );
+};
 
-    </div>
-  )
-}
-
-export default Home
+export default Home;
