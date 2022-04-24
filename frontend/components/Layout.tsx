a09491ceadff071778406fb69ac4ca2e70a9f89c
Nathan <nathancai1031@gmail.com>
2022-04-24 14:44:42 -0400

    added the 404 and faq page and updated links for nav bar

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -2,15 +2,25 @@ import Head from "next/head";
 import * as React from "react";
 import SearchBox from "./SearchBox";
 import style from "../styles/Layout.module.scss";
+import nav from "../styles/Nav.module.scss";
+import Link from "next/link";
 
 type Props = {
-    title: string,
+    title: string;
     children: React.ReactNode;
-}
+};
 
-const Layout:React.FC<Props> = ({ title, children }) => {
+const Layout: React.FC<Props> = ({ title, children }) => {
+    React.useEffect(() => {
+        const body = document.querySelector("body")!;
+        const html = document.querySelector("html")!;
+        body.style.margin = "0";
+        body.style.height = "100vh";
+        html.style.height = "100vh";
+    }, []);
+
     return (
-        <>
+        <div className={style["layout"]}>
             <Head>
                 <title>FoodFlation | {title} </title>
                 <meta
@@ -19,14 +29,15 @@ const Layout:React.FC<Props> = ({ title, children }) => {
                 />
                 <link rel="icon" href="/favicon.png" />
             </Head>
-            <nav>
-                <SearchBox />
-                <div>Home</div>
-                <div>FAQ</div>
+            <nav className={nav["nav"]}>
+                {/* <SearchBox /> */}
+                <Link href={"/"}>Home</Link>
+                <Link href={"/store/find"}>Search Prices</Link>
+                <Link href={"/faq"}>FAQ</Link>
             </nav>
-            <div className={style["layout"]}>{children}</div>
-        </>
+            <div>{children}</div>
+        </div>
     );
-}
+};
 
-export default Layout
+export default Layout;
