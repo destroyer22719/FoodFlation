8c6388f39fadeaf2162d40203847bae67744e06c
Nathan <nathancai1031@gmail.com>
2022-05-01 10:47:53 -0400

    refactored and made small adjustments to layout and nav components

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,41 +1,22 @@
 import Head from "next/head";
-import * as React from "react";
-import SearchBox from "./SearchBox";
+import React, {useEffect} from "react";
 import style from "../styles/Layout.module.scss";
 import nav from "../styles/Nav.module.scss";
 import Link from "next/link";
-import { Button } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { ButtonOutlined } from "../muiComponents";
 
 type Props = {
     title: string;
     children: React.ReactNode;
 };
 
-const buttonStyles = {
-    "&.MuiButton-root": {
-        border: "2px solid #9388A2",
-    },
-    "&.MuiButton-text": {
-        color: "#9388A2",
-    },
-    "&.MuiButton-contained": {
-        color: "#9388A2",
-    },
-    "&.MuiButton-outlined": {
-        color: "#9388A2",
-    },
-};
-
-const iconStyles = {
-    fill: "#9388A2"
-}
-
 const Layout: React.FC<Props> = ({ title, children }) => {
-    React.useEffect(() => {
+    useEffect(() => {
         const body = document.querySelector("body")!;
         const html = document.querySelector("html")!;
         body.style.margin = "0";
+        body.style.backgroundColor = "#0E050F";
         body.style.height = "100vh";
         html.style.height = "100vh";
     }, []);
@@ -43,7 +24,7 @@ const Layout: React.FC<Props> = ({ title, children }) => {
     return (
         <div className={style["layout"]}>
             <Head>
-                <title>FoodFlation | {title} </title>
+                <title>FoodFlation | {title || ""} </title>
                 <meta
                     name="description"
                     content="Track price histories of grocery stores in Canada"
@@ -53,22 +34,22 @@ const Layout: React.FC<Props> = ({ title, children }) => {
             <nav className={nav["nav"]}>
                 {/* <SearchBox /> */}
                 <Link href={"/"} passHref>
-                    <Button sx={buttonStyles} variant="outlined">
+                    <ButtonOutlined variant="outlined">
                         Home
-                    </Button>
+                    </ButtonOutlined>
                 </Link>
-                <Link href={"/store/find"} passHref>
-                    <Button sx={buttonStyles} variant="outlined">
-                        <SearchIcon sx={iconStyles} /> Track Prices
-                    </Button>
+                <Link href={"/store/"} passHref>
+                    <ButtonOutlined variant="outlined">
+                        <SearchIcon /> Track Prices
+                    </ButtonOutlined>
                 </Link>
                 <Link href={"/faq"} passHref>
-                    <Button sx={buttonStyles} variant="outlined">
+                    <ButtonOutlined variant="outlined">
                         FAQ
-                    </Button>
+                    </ButtonOutlined>
                 </Link>
             </nav>
-            <div>{children}</div>
+            <div className={style["layout__main"]}>{children}</div>
         </div>
     );
 };
