62c8191bbddbe6c99db86dc21432cd4e53326670
Nathan <nathancai1031@gmail.com>
2022-05-01 10:50:21 -0400

    Updated API URL to check if the environment var existed in the first place for prod

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -1,1 +1,1 @@
-export const API_URL = process.env.NODE_ENV === "production" ? process.env.API_URL : "http://localhost:4000";
+export const API_URL = process.env.NODE_ENV === "production" && process.env.API_URL ? process.env.API_URL : "http://localhost:4000";
