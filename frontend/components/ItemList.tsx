883de3ca375104bebfd8e7af89debfeca59dfb3d
Nathan <nathancai1031@gmail.com>
2022-05-01 10:50:50 -0400

    Fixed ItemList component

--- frontend/components/ItemList.tsx
+++ frontend/components/ItemList.tsx
@@ -1,25 +1,33 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Item } from "../global";
-import { sortItemPricesByDate } from "../utli";
 
 type Props = {
     item: Item;
 };
 
-const ItemList:React.FC<Props> = ({ item }) => {
-    item.prices = sortItemPricesByDate(item.prices);
-
+const ItemList: React.FC<Props> = ({ item }) => {
     return (
         <Link href={`/item/${item.id}`} passHref>
-            <a href="#">
+            <div>
                 <h3>{item.name}</h3>
-                <Image width={125} height={125} src={item.imgUrl} alt={`Image of ${item.name}`} />
-                <div>{"$"}{item.prices[0].price}</div>
-            <div>Last Updated: {new Date(item.prices[0].createdAt).toLocaleString("en-US")}</div>
-            </a>
+                <Image
+                    width={125}
+                    height={125}
+                    src={item.imgUrl}
+                    alt={`Image of ${item.name}`}
+                />
+                <div>
+                    {"$"}
+                    {item.prices[0].price}
+                </div>
+                <div>
+                    Last Updated:{" "}
+                    {new Date(item.prices[0].createdAt).toLocaleString("en-US")}
+                </div>
+            </div>
         </Link>
     );
-}
+};
 
-export default ItemList
+export default ItemList;
