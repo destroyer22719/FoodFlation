e16e27b78b546145542d94ccffae8ba1f78120aa
Nathan <nathancai1031@gmail.com>
2022-06-22 20:04:27 -0400

    added rate limiter on backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import sequelize from "./config/db.js";
 import morgan from "morgan";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 
 import itemRouter from "./routes/items.js";
 import storeRouter from "./routes/stores.js";
@@ -10,13 +11,20 @@ import companyRouter from "./routes/companies.js";
 
 const port = process.env.PORT || 4000;
 const dotEnvFile = process.env.NODE_ENV === "development" ? ".env" : "env.prod";
-dotenv.config({path: dotEnvFile});
+dotenv.config({ path: dotEnvFile });
 
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 app.use(cors());
 app.use(morgan("tiny"));
-
+app.use(limiter);
 
 app.use("/items", itemRouter);
 app.use("/stores", storeRouter);
