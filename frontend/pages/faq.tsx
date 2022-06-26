19324e46be0e47ca67e37e688eb96e91453f66d0
Nathan <nathancai1031@gmail.com>
2022-06-26 16:24:58 -0400

    updated the FAQ page

--- frontend/pages/faq.tsx
+++ frontend/pages/faq.tsx
@@ -28,12 +28,13 @@ const FaqPage: React.FC<{}> = () => {
                 Attemps have been made to scrape for prices on a larger pool of
                 grocery stores but {"it's"} difficult or impossible to do so for
                 some of them. One of the main reasons being that the grocery
-                {"store's"} website only contains flyers {"("}Freshco and NoFrills
+                {"store's"} website only contains flyers {"("}Freshco and
+                NoFrills
                 {")"} or there tools to shut down bots {"("}Walmart{")"}. While
                 more stores will hopefully be added the current stores are the
-                most ideal because {"it's"} website specifically lists the price of
-                all grocery store items by location. If you have a grocery store that
-                fits the criteria for scraping please contact me.
+                most ideal because {"it's"} website specifically lists the price
+                of all grocery store items by location. If you have a grocery
+                store that fits the criteria for scraping please contact me.
             </p>
             <h3>
                 Why are there only Canadian grocery store prices? Will you add
@@ -45,6 +46,21 @@ const FaqPage: React.FC<{}> = () => {
                 a program like this stores a lot of data. This is something I
                 hope to do in the next coming months after carefully observing
                 storage and costs.
+            </p>
+            <h3>How can I support FoodFlation?</h3>
+            <p>
+                For the most part I would appreciate if you can spread the word
+                about this website. It takes a lot of effort creating {"("}and
+                dugging{")"} a website like this. I don't use ads, a Patreon or
+                buymeacoffee account, paywalls, and user tracking tools such as Google
+                Analytics. Because I'm not using any using tracking software I
+                don't fully know how popular my website is, which is why I would
+                appreciate even a simple email, which is more than enough for me.
+                You can find my email at the footer on the bottom right side of
+                the site. Hearing the feedback would deeply encourage me to
+                spend more time on this project. If you would like to help out
+                you can submit a pull request on the site's github page. If you
+                would like to support in other ways please email me.
             </p>
         </Layout>
     );
