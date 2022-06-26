26d6b04d82b880727fe206a50d02c22b99e63e4e
Nathan <nathancai1031@gmail.com>
2022-06-26 11:45:40 -0400

    fixed MUI for NextJS on page refresh

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,28 @@
-import type { AppProps } from 'next/app'
+import * as React from "react";
+import type { AppProps } from "next/app";
+import { CacheProvider, EmotionCache } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import createEmotionCache from "../util/createEmotionCache";
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache;
 }
 
-export default MyApp
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
+    const {
+        Component,
+        emotionCache = clientSideEmotionCache,
+        pageProps,
+    } = props;
+
+    return (
+        <CacheProvider value={emotionCache}>
+            <CssBaseline />
+            <Component {...pageProps} />
+        </CacheProvider>
+    );
+};
+
+export default MyApp;
