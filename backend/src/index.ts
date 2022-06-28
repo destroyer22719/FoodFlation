46fbcca754db4bd2ed1cf1c63056fd5fe88cde5d
Nathan <nathancai1031@gmail.com>
2022-06-28 11:07:03 -0400

    added sentry

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,16 +4,37 @@ import sequelize from "./config/db.js";
 import morgan from "morgan";
 import cors from "cors";
 import rateLimit from "express-rate-limit";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import itemRouter from "./routes/items.js";
 import storeRouter from "./routes/stores.js";
 import companyRouter from "./routes/companies.js";
 
 const port = process.env.PORT || 4000;
-const dotEnvFile = process.env.NODE_ENV === "production" ? ".env.prod": ".env";
+const dotEnvFile = process.env.NODE_ENV === "production" ? ".env.prod" : ".env";
 dotenv.config({ path: dotEnvFile });
 
 const app = express();
+
+Sentry.init({
+    dsn: "https://f7f2f30e120647fc9cb732278d0b05f5@o455896.ingest.sentry.io/6536585",
+    integrations: [
+        // enable HTTP calls tracing
+        new Sentry.Integrations.Http({ tracing: true }),
+        // enable Express.js middleware tracing
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000,
@@ -29,6 +50,11 @@ app.use(limiter);
 app.use("/items", itemRouter);
 app.use("/stores", storeRouter);
 app.use("/companies", companyRouter);
+app.get("/debug-sentry", (_req, _res) => {
+    throw new Error("My first Sentry error!");
+});
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.listen(port, async () => {
     await sequelize.sync();
