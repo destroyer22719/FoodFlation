6fed6c988eae13dcaefb95dd18dc8d35e4e54423
Nathan <nathancai1031@gmail.com>
2022-07-13 12:43:26 -0400

    added meta tags for SEO

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -27,8 +27,11 @@ const Layout: React.FC<Props> = ({ title, children }) => {
                 <title>FoodFlation | {title || ""} </title>
                 <meta
                     name="description"
-                    content="Track price histories of grocery stores in Canada"
+                    content="Track price histories of grocery stores across Canada"
                 />
+                <meta name="robots" content="index, follow" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                <meta charSet="UTF-8"/>
                 <link rel="icon" href="/favicon.png" />
             </Head>
             <Nav />
--- frontend/pages/about.tsx
+++ frontend/pages/about.tsx
@@ -4,7 +4,7 @@ const AboutPage: React.FC = () => {
             <h1>About Food Flation</h1>
             <div>
                 Food flation is made to track price history of items in a variety of
-                different grocery stores accross Canada from companies including Loblaws
+                different grocery stores across Canada from companies including Loblaws
             </div>
         </div>
     );
