6f47bf5983c5c8d10b0c501e52d1ea6581715722
Nathan <nathancai1031@gmail.com>
2022-07-14 10:11:36 -0400

    fixed sentry issues

--- frontend/config/index.ts
+++ frontend/config/index.ts
@@ -2,3 +2,4 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const API_URL = process.env.API_URL ? process.env.API_URL : "http://localhost:4000";
+export const SENTRY_DSN = process.env.SENTRY_DSN ? process.env.SENTRY_DSN : "";
--- frontend/pages/debug/index.tsx
+++ frontend/pages/debug/index.tsx
@@ -1,22 +1,22 @@
-import { GetServerSideProps } from "next";
+// import { GetServerSideProps } from "next";
 import React, {useEffect} from "react";
 
-const index: React.FC<null> = () => {
-    return <div>This should return an error</div>;
-};
-
-export const getServerSideProps: GetServerSideProps = async () => {
-    await fetch("localhost:4000/fail");
-    return { props: {} };
-};
-
-export default index;
-
-// export default function index() {
-//     useEffect(() => {
-//         (async () => {
-//             await fetch("localhost:4000/fail")
-//         })();
-//     }, []);
+// const index: React.FC<null> = () => {
 //     return <div>This should return an error</div>;
-// }
+// };
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//     await fetch("localhost:4000/fail");
+//     return { props: {} };
+// };
+
+// export default index;
+
+export default function index() {
+    useEffect(() => {
+        (async () => {
+            await fetch("localhost:4000/fail")
+        })();
+    }, []);
+    return <div>This should return an error</div>;
+}
