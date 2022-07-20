cb57f47dd31f4036ac228da8775b5cbe2d1dfbbd
Nathan <nathancai1031@gmail.com>
2022-07-20 09:20:19 -0400

    added sitemaps generator

---
+++ frontend/next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: "https://foodflation.me",
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
+  
+  export default config
