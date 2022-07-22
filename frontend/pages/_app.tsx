439025ab46a9c48077546f02c3dee52f5e17d067
Nathan <nathancai1031@gmail.com>
2022-07-22 10:15:44 -0400

    reinserted TinyAnalytics fix

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -19,26 +19,26 @@ const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
     } = props;
     const router = useRouter();
 
-    // useEffect(() => {
-    //     // since this is a SPA TinyAnalytics won't work after the first page load or refresh
-    //     // this code makes it so it deletes the script tag and inserts the script tag again on route changes
-    //     const handleRouteChange = () => {
-    //         const head = document.querySelector("head")!;
-    //         const originalScript = document.getElementById("tinyAnalytics")!;
-    //         originalScript.remove();
-    //         const script = document.createElement("script");
-    //         script.id = "tinyAnalytics";
-    //         script.defer = true;
-    //         script.src = process.env.NEXT_PUBLIC_TINYANALYTICS_URI || "";
-    //         head.appendChild(script);
-    //     };
+    useEffect(() => {
+        // since this is a SPA TinyAnalytics won't work after the first page load or refresh
+        // this code makes it so it deletes the script tag and inserts the script tag again on route changes
+        const handleRouteChange = () => {
+            const head = document.querySelector("head")!;
+            const originalScript = document.getElementById("tinyAnalytics")!;
+            originalScript.remove();
+            const script = document.createElement("script");
+            script.id = "tinyAnalytics";
+            script.defer = true;
+            script.src = process.env.NEXT_PUBLIC_TINYANALYTICS_URI || "";
+            head.appendChild(script);
+        };
 
-    //     router.events.on("routeChangeStart", handleRouteChange);
+        router.events.on("routeChangeStart", handleRouteChange);
 
-    //     return () => {
-    //         router.events.off("routeChangeStart", handleRouteChange);
-    //     };
-    // }, []);
+        return () => {
+            router.events.off("routeChangeStart", handleRouteChange);
+        };
+    }, []);
 
     return (
         <CacheProvider value={emotionCache}>
