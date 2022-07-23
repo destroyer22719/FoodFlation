52fefe1926c951bc4762302ea1b25d57d3e2e0cb
Nathan <nathancai1031@gmail.com>
2022-07-23 13:03:43 -0400

    updated date formating to exclude time

--- frontend/components/ItemList.tsx
+++ frontend/components/ItemList.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { Item } from "../global";
 import styles from "../styles/Store.module.scss";
 import ButtonOutlined from "./ButtonOutlined";
@@ -9,32 +10,32 @@ type Props = {
 };
 
 const ItemList: React.FC<Props> = ({ item }) => {
+    const [date, setDate] = useState<string | null>(null);
+    useState(() => {
+        setDate(new Date(item.lastUpdated!).toISOString().split("T")[0]);
+    });
+
     return (
-            <ButtonOutlined className={styles["store-page__item"]}>
-                <Link href={`/item/${item.id}`} passHref>
-                    <a href="#">
-                        <div>
-                            <h3>{item.name}</h3>
-                            <Image
-                                width={125}
-                                height={125}
-                                src={item.imgUrl}
-                                alt={`Image of ${item.name}`}
-                            />
-                            <div className={styles["store-page__item-price"]}>
-                                {"$"}
-                                {item.price}
-                            </div>
-                            <div>
-                                Last Updated:{" "}
-                                {item.lastUpdated ? new Date(item.lastUpdated).toLocaleString(
-                                    "en-US"
-                                ): "N/A"}
-                            </div>
+        <ButtonOutlined className={styles["store-page__item"]}>
+            <Link href={`/item/${item.id}`} passHref>
+                <a href="#">
+                    <div>
+                        <h3>{item.name}</h3>
+                        <Image
+                            width={125}
+                            height={125}
+                            src={item.imgUrl}
+                            alt={`Image of ${item.name}`}
+                        />
+                        <div className={styles["store-page__item-price"]}>
+                            {"$"}
+                            {item.price}
                         </div>
-                    </a>
-                </Link>
-            </ButtonOutlined>
+                        <div>Last Updated: {date}</div>
+                    </div>
+                </a>
+            </Link>
+        </ButtonOutlined>
     );
 };
 
