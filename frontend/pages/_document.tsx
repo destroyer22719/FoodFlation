768de436fd71eacde959a640fcfdd80e9cb0d03c
Nathan <nathancai1031@gmail.com>
2022-07-27 13:28:30 -0400

    implemented another attempt to fix tiny analytics

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -13,6 +13,13 @@ export default class MyDocument extends Document {
                         rel="stylesheet"
                         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                     />
+                    <script
+                    type="text/javascript"
+                    async
+                    id="tinyAnalytics"
+                        defer
+                        src={process.env.NEXT_PUBLIC_TINYANALYTICS_URI}
+                    ></script>
                 </Head>
                 <body>
                     <Main />
