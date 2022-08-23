39779256f5e6ca6619ba6e0bddbcd009dcee500d
Nathan <nathancai1031@gmail.com>
2022-08-23 11:25:33 -0400

    removed sequelize migration code in config

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,5 @@
 import { Sequelize } from "sequelize-typescript";
 import dotenv from "dotenv";
-import { SequelizeTypescriptMigration } from "sequelize-typescript-migration-v2";
-import path from "path";
 import Item from "../model/Item.js";
 import Price from "../model/Price.js";
 import Store from "../model/Store.js";
@@ -24,12 +22,5 @@ const sequelize = new Sequelize({
     host: process.env.DATABASE_HOST || "localhost",
     logging: false,
 });
-
-//@ts-ignore - this thing works even though it says the type is not compatible
-(async () => await SequelizeTypescriptMigration.makeMigration(sequelize, {
-	outDir: path.join(__dirname, "../", "../", "/migrations"),
-	migrationName: "migration",
-	preview: false,
-}))();
 
 export default sequelize;
