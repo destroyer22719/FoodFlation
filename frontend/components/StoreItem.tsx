c3597444ee19f09aeb0411ebc652ab535587550b
Nathan <nathancai1031@gmail.com>
2022-12-24 16:30:38 -0500

    added aldi logo and fixed error on store list page

--- frontend/components/StoreItem.tsx
+++ frontend/components/StoreItem.tsx
@@ -5,36 +5,37 @@ import ButtonContained from "./CustomButtonComponents/ButtonContained";
 import style from "../styles/StoreList.module.scss";
 
 type Props = {
-    store: Store;
+  store: Store;
 };
 
 const StoreItem: React.FC<Props> = ({ store }) => {
-    return (
-        <div className={style["store-list__store-item"]}>
-            <ButtonContained className={style["store-list__store-item-btn"]}>
-                <Link
-                    href={`/store/${store.id}`}
-                    passHref
-                    className={style["store-list__store-item--format"]}
-                >
-                    <div className={style["store-list__store-item-image"]}>
-                        <Image
-                            src={`/store-logos/${store.name
-                                .toLocaleLowerCase()
-                                .replaceAll(" ", "_")}-logo.png`}
-                            alt={store.name}
-                            layout="fill"
-                        />
-                    </div>
-                    <div className={style["store-list__store-item-address"]}>
-                        <span>{store.street}</span>
-                        <span>{store.city}</span>
-                        <span>{store.postalCode}</span>
-                    </div>
-                </Link>
-            </ButtonContained>
-        </div>
-    );
+  return (
+    <div className={style["store-list__store-item"]}>
+      <ButtonContained className={style["store-list__store-item-btn"]}>
+        <Link
+          href={`/store/${store.id}`}
+          className={style["store-list__store-item--format"]}
+        >
+          <>
+            <div className={style["store-list__store-item-image"]}>
+              <Image
+                src={`/store-logos/${store.name
+                  .toLocaleLowerCase()
+                  .replaceAll(" ", "_")}-logo.png`}
+                alt={store.name}
+                layout="fill"
+              />
+            </div>
+            <div className={style["store-list__store-item-address"]}>
+              <span>{store.street}</span>
+              <span>{store.city}</span>
+              <span>{store.postalCode}</span>
+            </div>
+          </>
+        </Link>
+      </ButtonContained>
+    </div>
+  );
 };
 
 export default StoreItem;
