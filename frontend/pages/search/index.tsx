7c14959475c8edfc100bdbd842f952e521a91d9e
Nathan <nathancai1031@gmail.com>
2022-12-26 21:35:07 -0500

    added search page

---
+++ frontend/pages/search/index.tsx
@@ -0,0 +1,12 @@
+import Layout from "../../components/Layout";
+
+const SearchPage = () => {
+  return (
+    <Layout title="Search Items">
+      <h1>Search Items</h1>
+      <h3>Find the price of items accross all stores in your city!</h3>
+    </Layout>
+  );
+};
+
+export default SearchPage;
