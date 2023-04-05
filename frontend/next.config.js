e6fccd7e381fd227e272903ca36a799f1e07885c
Nathan Cai <nathancai1031@gmail.com>
2023-04-05 17:17:03 -0400

    added next bundle analyzer

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,35 +5,41 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 const { withSentryConfig } = require("@sentry/nextjs");
-
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 const moduleExports = {
-    reactStrictMode: true,
-    images: {
-        domains: [
-            "assets.shop.loblaws.ca",
-            "assets.sellers.loblaw.ca",
-            "product-images.metro.ca",
-            "www.metro.ca",
-            "m.media-amazon.com",
-            "www.instacart.com",
-            "upload.wikimedia.org",
-            "target.scene7.com"
-        ],
-    },
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "assets.shop.loblaws.ca",
+      "assets.sellers.loblaw.ca",
+      "product-images.metro.ca",
+      "www.metro.ca",
+      "m.media-amazon.com",
+      "www.instacart.com",
+      "upload.wikimedia.org",
+      "target.scene7.com",
+    ],
+  },
 };
 
 const sentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
 
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withPlugins(
+  [[withBundleAnalyzer], [withSentryConfig, sentryWebpackPluginOptions]],
+  moduleExports
+);
