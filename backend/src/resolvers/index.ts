c4d17b55a1b56295ef37d230045538d4995a4584
Nathan Cai <nathancai1031@gmail.com>
2023-04-25 17:01:33 -0400

    successfully migrated from expreess to apollo

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -1,16 +1,17 @@
-import { DateTimeResolver, UUIDResolver } from "graphql-scalars";
 import { locationsResolver, storeResolver, storesResolver } from "./stores.js";
 import { companyResolver } from "./company.js";
 import { itemStoreResolver, itemCityResolver, itemResolver } from "./item.js";
 
 export const root = {
-  UUID: UUIDResolver,
-  DateTime: DateTimeResolver,
   store: storeResolver,
   stores: storesResolver,
   company: companyResolver,
   item: itemResolver,
   itemsFromStore: itemStoreResolver,
   itemsFromCity: itemCityResolver,
-  locations: locationsResolver
+  locations: locationsResolver,
+};
+
+export const resolvers = {
+  Query: { ...root },
 };
---
+++ backend/src/server.ts
@@ -0,0 +1,18 @@
+import { ApolloServer } from "@apollo/server";
+import {
+  startServerAndCreateLambdaHandler,
+  handlers,
+} from "@as-integrations/aws-lambda";
+
+import { resolvers } from "./resolvers/index.js";
+import { typeDefs } from "./model/schema.js";
+
+const server = new ApolloServer({
+  typeDefs: typeDefs,
+  resolvers: resolvers,
+});
+
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server,
+  handlers.createAPIGatewayProxyEventV2RequestHandler()
+);
