ae7356a703d95557959a68143a53a696b791aadc
Nathan Cai <nathancai1031@gmail.com>
2023-04-26 21:51:31 -0400

    updated mui mixin

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -8,23 +8,23 @@ const Nav = () => {
   return (
     <nav className={styles["nav"]}>
       <div className={styles["nav__section"]}>
-        <Link href={"/"} className={styles["nav__item"]}>
+        <Link href={"#"} className={styles["nav__item"]}>
           Home
         </Link>
       </div>
       <div className={styles["nav__item--center"]}>
-        <Link href={"/store/"} className={styles["nav__item"]}>
+        <Link href={"#"} className={styles["nav__item"]}>
           <MdSearch /> Search Stores
         </Link>
-        <Link href={"/search/"} className={styles["nav__item"]}>
+        <Link href={"#"} className={styles["nav__item"]}>
           <MdSearch /> Search Items
         </Link>
       </div>
       <div className={styles["nav__section"]}>
-        <Link href={"/faq"} className={styles["nav__item"]}>
+        <Link href={"#"} className={styles["nav__item"]}>
           FAQ
         </Link>
-        <Link href={"/about"} className={styles["nav__item"]}>
+        <Link href={"#"} className={styles["nav__item"]}>
           About
         </Link>
       </div>
