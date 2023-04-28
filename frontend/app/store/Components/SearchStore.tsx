6cb9fb16b0668b1e486e0246564d849fbdd3287a
Nathan Cai <nathancai1031@gmail.com>
2023-04-28 01:08:27 -0400

    implemented clearer warning messages for search stores by postal codes and zip codes

--- frontend/app/store/Components/SearchStore.tsx
+++ frontend/app/store/Components/SearchStore.tsx
@@ -4,40 +4,61 @@ import { useState } from "react";
 import { MdOutlineLocationSearching } from "react-icons/md";
 
 import styles from "@/styles/Components/SearchStore.module.scss";
+import { useRouter } from "next/navigation";
 
 const SearchStore = () => {
+  const router = useRouter();
   const [codeInput, setCodeInput] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
 
-  const postalCodeRegex = /^[A-Z]\d[A-Z][ ]\d[A-Z]\d$/;
-  const zipCodeRegex = /^\d{5}$/;
-
-  const inputHandler = (codeInput: string) => {
-    codeInput = codeInput.toUpperCase();
-    if (postalCodeRegex.test(codeInput) || zipCodeRegex.test(codeInput)) {
-      setCodeInput(codeInput);
+  const searchByCode = () => {
+    const postalCodeRegex = /^[A-Z]\d[A-Z][ ]\d[A-Z]\d$/;
+    const zipCodeRegex = /^\d{5}$/;
+    if (postalCodeRegex.test(codeInput)) {
+      router.push(`/store?postalCode=${codeInput}`);
+    } else if (zipCodeRegex.test(codeInput)) {
+      router.push(`/store?zipCode=${codeInput}`);
+    } else {
+      setShowWarning(true);
+      setTimeout(() => {
+        setShowWarning(false);
+      }, 5000);
     }
   };
 
   return (
     <div className={styles["search-stores"]}>
       <div className={styles["search-stores__section"]}>
-        <div className={styles["search-stores__icon"]}>
+        <div
+          className={styles["search-stores__icon"]}
+          onClick={() => searchByCode()}
+        >
           <MdOutlineLocationSearching />
         </div>
         <input
           className={styles["search-stores__input"]}
           value={codeInput}
           placeholder="A1A 1A1 or 12345"
-          onChange={(e) => inputHandler(e.target.value)}
+          onChange={(e) => setCodeInput(e.target.value.toUpperCase())}
           onKeyDown={(e) => {
             if (e.key === "Enter") {
+              searchByCode();
             }
           }}
         />
       </div>
-      <div className={styles["search-stores__disclaimer"]}>
-        {codeInput &&
-          "This search functions only searches by exact postal codes or zip codes, not the nearest stores to it"}
+      <div>
+        {showWarning && (
+          <div className={styles["search-stores__warning"]}>
+            Please enter a valid postal code or zip code
+          </div>
+        )}
+        {codeInput && (
+          <div className={styles["search-stores__warning"]}>
+            This search functions only searches by exact postal codes or zip
+            codes, not the nearest stores to it
+          </div>
+        )}
       </div>
     </div>
   );
