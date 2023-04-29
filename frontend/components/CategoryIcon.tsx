529b70df0b44d4ff44da56ed46434e3da6d74218
Nathan Cai <nathancai1031@gmail.com>
2023-04-29 15:54:42 -0400

    added category icon component

--- frontend/components/CategoryIcon.tsx
+++ frontend/components/CategoryIcon.tsx
@@ -1,9 +1,43 @@
+import { GiSteak, GiFruitBowl, GiMilkCarton } from "react-icons/gi";
+import { BiBowlHot } from "react-icons/bi";
+
+import styles from "@/styles/CategoryIcon.module.css";
+
 type Category = {
   category: string;
 };
 
 const CategoryIcon: React.FC<Category> = ({ category }) => {
-  return <div></div>;
+  const categories = [
+    "Meat",
+    "Fruits & Vegetables",
+    "Starches & Grains",
+    "Dairy",
+    "Miscellaneous",
+  ];
+  let categoryClassName = category.toLowerCase().replace(/\s/g, "-");
+
+  if (!categories.includes(category)) {
+    categoryClassName = "miscellaneous";
+  }
+  return (
+    <div className={styles[`category__${categoryClassName}`]} title={category}>
+      {(() => {
+        switch (category) {
+          case "Meat":
+            return <GiSteak />;
+          case "Fruits & Vegetables":
+            return <GiFruitBowl />;
+          case "Starches & Grains":
+            return <GiFruitBowl />;
+          case "Dairy":
+            return <GiMilkCarton />;
+          default:
+            return <BiBowlHot />;
+        }
+      })()}
+    </div>
+  );
 };
 
 export default CategoryIcon;
