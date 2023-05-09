a07b8dd96269b3127c5b6fa8c5762fb9ae4d4081
Nathan <nathancai1031@gmail.com>
2023-05-09 16:31:34 -0400

    renamed to category list

---
+++ frontend/app/(store)/store/[id]/Components/CategoriesList.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname, useSearchParams } from "next/navigation";
+import { RxCross2 } from "react-icons/rx";
+
+type Props = {
+  categoriesData: {
+    category: string;
+    count: number;
+  }[];
+};
+
+const Categories = ({ categoriesData }: Props) => {
+  const searchParams = useSearchParams();
+  const categorySelected = searchParams.get("category");
+  const pathName = usePathname();
+
+  return (
+    <div>
+      {categorySelected && (
+        <div>
+          <RxCross2 />
+        </div>
+      )}
+      <div>
+        {categoriesData.map(({ count, category }) => (
+          <Link href={`${pathName}?category=${category}`} key={category}>
+            <div>
+              {count} - {category}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
