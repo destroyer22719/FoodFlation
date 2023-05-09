dd0b07000ec5cd26ee2b265c52b0c99592f4b014
Nathan <nathancai1031@gmail.com>
2023-05-09 19:25:14 -0400

    gave up on trying to fix redis reconnecting for now

--- backend/src/db/redis.ts
+++ backend/src/db/redis.ts
@@ -7,7 +7,7 @@ const redisClient = new Redis(process.env.REDIS_URL as string);
 
 redisClient.on("connecting", () => {
   console.log("Initialized Redis connection");
-})
+});
 
 redisClient.on("connect", () => {
   console.log("Redis connected");
@@ -21,8 +21,12 @@ redisClient.on("error", (err) => {
   console.log("Redis error: ", err);
 });
 
+redisClient.on("reconnecting", () => {
+  console.log("Redis reconnecting");
+});
+
 redisClient.on("close", () => {
   console.log("Redis closed");
 });
 
-export default redisClient;
+export default redisClient as Redis;
