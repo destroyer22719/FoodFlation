a4298d1fbc41e37c77d2a16a8ce2b211f09b5102
Nathan <nathancai1031@gmail.com>
2023-05-11 20:45:14 -0400

    implemented million js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// const { withSentryConfig } = require("@sentry/nextjs");
 let withBundleAnalyzer;
 if (process.env.NODE_ENV !== "production") {
   withBundleAnalyzer = require("@next/bundle-analyzer")({
@@ -12,9 +7,10 @@ if (process.env.NODE_ENV !== "production") {
   });
 }
 
+const million = require("million/compiler");
+
 const moduleExports = {
   reactStrictMode: true,
-  experimental: { appDir: true },
   images: {
     domains: [
       "assets.shop.loblaws.ca",
@@ -29,23 +25,8 @@ const moduleExports = {
   },
 };
 
-// const sentryWebpackPluginOptions = {
-//   // Additional config options for the Sentry Webpack plugin. Keep in mind that
-//   // the following options are set automatically, and overriding them is not
-//   // recommended:
-//   //   release, url, org, project, authToken, configFile, stripPrefix,
-//   //   urlPrefix, include, ignore
-
-//   silent: true, // Suppresses all logs
-//   // For all available options, see:
-//   // https://github.com/getsentry/sentry-webpack-plugin#options.
-// };
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-// module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
 if (process.env.NODE_ENV !== "production") {
   module.exports = withBundleAnalyzer(moduleExports);
 } else {
-  module.exports = moduleExports;
+  module.exports = million.next(moduleExports);
 }
