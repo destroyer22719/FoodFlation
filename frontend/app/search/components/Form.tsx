3a9f5a2e596498b260fe076d11aff7d4d995c096
Nathan <nathancai1031@gmail.com>
2023-05-13 13:38:00 -0400

    implemented location inputs for form of search page

--- frontend/app/search/components/Form.tsx
+++ frontend/app/search/components/Form.tsx
@@ -17,6 +17,7 @@ const Form: React.FC<Props> = ({
   state: stateProp,
   city: cityProp,
   search: searchProp,
+  locations,
 }) => {
   const [country, setCountry] = useState<"" | "Canada" | "United States">(
     countryProp === "Canada" || countryProp === "United States"
@@ -24,19 +25,91 @@ const Form: React.FC<Props> = ({
       : ""
   );
 
-  const province = useState(provinceProp);
-  const state = useState(stateProp);
-  const city = useState(cityProp);
-  const search = useState(searchProp);
+  const provinceList = locations["Canada"].provinces;
+  const stateList = locations["United States"].states;
+
+  const containsProv =
+    provinceProp && country === "Canada" && provinceList.includes(provinceProp);
+
+  const containsState =
+    stateProp && country === "United States" && stateList.includes(stateProp);
+
+  const [province, setProvince] = useState(containsProv ? provinceProp : "");
+  const [state, setState] = useState(containsState ? stateProp : "");
+
+  const containsCity = () => {
+    if (!cityProp) return false;
+    if (country === "Canada" && province) {
+      return locations["Canada"][province].includes(cityProp);
+    } else if (country === "United States" && state) {
+      return locations["United States"][state].includes(cityProp);
+    }
+    return false;
+  };
+
+  const [cities, setCities] = useState([] as string[]);
+  const [city, setCity] = useState(containsCity() ? cityProp : "");
+  const [search, setSearch] = useState(searchProp || "");
 
   return (
     <div>
       <div>
-        <label htmlFor="country">Country</label>
+        <h3>Country</h3>
         <div>
           <div onClick={() => setCountry("Canada")}>Canada</div>
           <div onClick={() => setCountry("United States")}>United States</div>
         </div>
+      </div>
+      <div>
+        {country && (
+          <div>
+            {country === "Canada" ? (
+              <div>
+                <h3>Provinces</h3>
+                <div>
+                  {provinceList.map((prov) => (
+                    <div
+                      onClick={() => {
+                        setProvince(prov);
+                        setCities(locations[country][prov]);
+                      }}
+                    >
+                      {prov}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            ) : (
+              <div>
+                <h3>States</h3>
+                <div>
+                  {stateList.map((st) => (
+                    <div
+                      onClick={() => {
+                        setState(st);
+                        setCities(locations[country][st]);
+                      }}
+                    >
+                      {st}
+                    </div>
+                  ))}
+                </div>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      <div>
+        {(province || state) && (
+          <div>
+            <h3>Cities</h3>
+            <div>
+              {cities.map((cty) => (
+                <div onClick={() => setCity(cty)}>{cty}</div>
+              ))}
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
