abaf78f403faddd1a14345de1e36207f2abaa996
Nathan <nathancai1031@gmail.com>
2023-05-17 14:36:15 -0400

    added faq and about page

---
+++ frontend/app/about/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About FoodFlation</h1>
+      <div>
+        FoodFlation is a side project I made in 2021 as food prices start to
+        soar. I made this with the intent to help consumers better understand
+        the inflation around them as well as help navigate through inflation by
+        finding the cheapest products
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ frontend/app/faq/page.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+
+const FaqPage = () => {
+  return (
+    <div>
+      <h2>How does FoodFlation get the data on item prices?</h2>
+      <div>
+        FoodFlation gets store data from web scraping store sites, the scraper
+        is made using NodeJS and Puppeteer
+      </div>
+      <h2>Why doesn't FoodFlation include other stores?</h2>
+      <div>
+        So far, the stores currently being scraped are stores with websites that
+        contains information about its store prices, and, most importantly,
+        doesn't employ tools to shut down web scrapers, such as Walmart.
+      </div>
+      <h2>Does FoodFlation collect any personal information?</h2>
+      <div>
+        FoodFlation doesn't store user information and it absoloutely doesn't
+        sell them to third parties. FoodFlation does use 2 third party tools to
+        help improve the site,{" "}
+        <Link
+          href={"https://vercel.com/docs/concepts/analytics/privacy-policy"}
+        >
+          Vercel Analytics
+        </Link>
+        and <Link href={"https://sentry.io/privacy/"}>Sentry</Link>
+      </div>
+      <h2>What tech stack does FoodFlation use</h2>
+      <div>
+        Prior to v2, FoodFlation uses NextJS, Express, Sequelize, MySQL. In v2,
+        FoodFlation uses NextJS13's new app directory, Prisma, Apollo GraphQL,
+        and MySQL. FoodFlation's rewrite in tech stacks help create a
+        performance, maintainable, and modern code base.
+      </div>
+    </div>
+  );
+};
+
+export default FaqPage;
