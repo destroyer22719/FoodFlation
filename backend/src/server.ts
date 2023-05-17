ca77bd2e71f5ef49c57e47ff32965b18bc314bfb
Nathan <nathancai1031@gmail.com>
2023-05-17 13:58:52 -0400

    implemented sentry?

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,12 +8,17 @@ import { KeyvAdapter } from "@apollo/utils.keyvadapter";
 import dotenv from "dotenv";
 import responseCachePlugin from "@apollo/server-plugin-response-cache";
 import KeyvRedis from "@keyv/redis";
+import * as Sentry from "@sentry/serverless";
 
 import { schema } from "./model/schema.js";
 import { context } from "./db/context.js";
 import redisClient from "./db/redis.js";
 
 dotenv.config();
+
+Sentry.AWSLambda.init({
+  dsn: process.env.SENTRY_DSN,
+});
 
 const keyV = new Keyv({
   store: new KeyvRedis(redisClient),
@@ -25,12 +30,14 @@ const server = new ApolloServer({
   plugins: [responseCachePlugin()],
 });
 
-export const graphqlHandler = startServerAndCreateLambdaHandler(
-  server,
-  handlers.createAPIGatewayProxyEventV2RequestHandler(),
-  {
-    context: async (handler) => {
-      return context(handler.event.requestContext.http.sourceIp);
-    },
-  }
+export const graphqlHandler = Sentry.AWSLambda.wrapHandler(
+  startServerAndCreateLambdaHandler(
+    server,
+    handlers.createAPIGatewayProxyEventV2RequestHandler(),
+    {
+      context: async (handler) => {
+        return context(handler.event.requestContext.http.sourceIp);
+      },
+    }
+  )
 );
