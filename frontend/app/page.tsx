ee8d3a61d938e0afc7adecb021cab62e4ceae07b
Nathan <nathancai1031@gmail.com>
2023-05-18 14:28:36 -0400

    finished home page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,22 @@
 import style from "@/styles/pages/Index.module.scss";
 import { getCounts } from "../graphql/queries";
 
+import styles from "@/styles/pages/Index.module.scss";
+
 const HomePage = async () => {
   const { itemCount, storeCount } = await getCounts();
 
   return (
-    <>
-      <div className={style["home"]}>
+    <div className={style["home"]}>
+      <div>
         <h1>FoodFlation</h1>
-        <p>
-          Tracking the prices of {itemCount} items from {storeCount} different stores
-          across Canada and the US!
-        </p>
+        <div>Helping consumers undersand and navigate food inflation</div>
+        <div className={styles["home__stats"]}>
+          <div className={styles["home__stat"]}>{itemCount} items</div>
+          <div className={styles["home__stat"]}>{storeCount} stores</div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 
