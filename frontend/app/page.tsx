58695695d18654ff1ec60a829a1eada3f019a357
Nathan <nathancai1031@gmail.com>
2023-06-05 21:53:56 -0400

    added the feature for the stats on the home page to animate

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,26 @@
-import style from "@/styles/pages/Index.module.scss";
-import { getCounts } from "../graphql/queries";
-
-import styles from "@/styles/pages/Index.module.scss";
 import { Metadata } from "next";
+
+import { getCounts } from "@/graphql/queries";
+import styles from "@/styles/pages/Index.module.scss";
+import Stats from "./components/stats";
 
 export const metadata: Metadata = {
   title: "FoodFlation | Home",
-}
+};
 
 const HomePage = async () => {
   const { itemCount, storeCount } = await getCounts();
 
   return (
-    <div className={style["home"]}>
+    <div className={styles["home"]}>
       <div>
         <h1>FoodFlation</h1>
         <div>Helping consumers undersand and navigate food inflation</div>
-        <div className={styles["home__stats"]}>
+        <Stats itemTotal={itemCount} storeTotal={storeCount} />
+        {/* <div className={styles["home__stats"]}>
           <div className={styles["home__stat"]}>{itemCount} items</div>
           <div className={styles["home__stat"]}>{storeCount} stores</div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
